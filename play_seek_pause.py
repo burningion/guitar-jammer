9d26fa3606cbccd97b4dbdb5dbf1933815d56fad
Kirk Kaiser <kirk@zothcorp.com>
2020-04-29 20:50:42 -0400

    Okay, I can control Spotify playback with my midi device

--- play_seek_pause.py
+++ play_seek_pause.py
@@ -3,8 +3,35 @@ from rtmidi.midiutil import open_midiinput
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-from time import sleep
+import time
 import os
+
+class MidiInputHandler(object):
+    def __init__(self, port, freq):
+        self.port = port
+        self.base_freq = freq
+        self._wallclock = time.time()
+
+    def __call__(self, event, data=None):
+        global currentFrequency
+        message, deltatime = event
+        self._wallclock += deltatime
+        print("[%s] @%0.6f %r" % (self.port, self._wallclock, message))
+        if (message[1] == 41) and (message[2] == 127):
+            try:
+                sp.start_playback()
+            except Exception as e:
+                print(e)
+        if (message[1] == 43) and (message[2] == 127):
+            try:
+                sp.seek_track(0)
+            except Exception as e:
+                print(e)
+        if (message[1] == 42) and (message[2] == 127):
+            try:
+                sp.pause_playback()
+            except Exception as e:
+                print(e)
 
 client_credentials_manager = SpotifyClientCredentials()
 
@@ -13,10 +40,15 @@ client_credentials_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(auth=os.environ['SPOTIFY_OAUTH_TOKEN'],
                      client_credentials_manager=client_credentials_manager)
 
-sp.start_playback()
-sleep(10)
-sp.seek_track(0)
-sleep(10)
-sp.pause_playback()
-import IPython
-IPython.embed()
+try:
+    midiin, port_name = open_midiinput(0)
+except (EOFError, KeyboardInterrupt):
+    exit()
+
+midiSettings = MidiInputHandler(port_name, 940.0)
+midiin.set_callback(midiSettings)
+
+
+while True:
+    time.sleep(.1)
+
